e0e93d9825b742ab7e444d188ce757b1cf272a7b
Neo Fu <rtugeek@gmail.com>
2024-05-04 00:07:22 +0800

    日历样式优化

--- src/widgets/calendar/CalendarDay.vue
+++ src/widgets/calendar/CalendarDay.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
+import { Solar } from 'lunar-typescript'
+import type { PropType } from 'vue'
+import { computed } from 'vue'
 
+const props = defineProps({
+  day: {
+    type: Object as PropType<Solar>,
+    required: true,
+  },
+})
+
+const isToday = computed(() => {
+  const todaySolar = Solar.fromDate(new Date())
+  if (props.day.getYear() === todaySolar.getYear() && props.day.getMonth() === todaySolar.getMonth() && props.day.getDay() === todaySolar.getDay()) {
+    return true
+  }
+  return false
+})
+
+const festivals = computed(() => {
+  return props.day.getFestivals()
+})
 </script>
 
 <template>
-  <div />
+  <div
+    class="flex flex-col items-center content-center rounded-full  size-10" :class="{
+      'bg-blue-400': isToday,
+      'text-white': isToday
+    }"
+  >
+    <div class="font-bold">
+      {{ day.getDay() }}
+    </div>
+    <div style="font-size: 10px" class="text-xs text-center">
+      <span v-if="festivals.length > 0" class="text-blue-400">
+        {{ festivals[0] }}
+      </span>
+      <span v-else class="opacity-50">
+        {{ day.getLunar().getDayInChinese() }}
+      </span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/widgets/calendar/CalendarWidgetView.vue
+++ src/widgets/calendar/CalendarWidgetView.vue
@@ -5,38 +5,48 @@ import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { ref } from 'vue'
 import { useWidget } from '@widget-js/vue3'
 import { WidgetData } from '@widget-js/core'
+import { Left, Right } from '@icon-park/vue-next'
+import CalendarDay from '@/widgets/calendar/CalendarDay.vue'
 
 dayjs.extend(weekOfYear)
 const now = dayjs()
+const currentMonth = ref(now.month() + 1)
 const solarMonth = SolarMonth.fromYm(now.year(), now.month() + 1)
-const weeks = ref(solarMonth.getWeeks(1))
+const weeks = ref(solarMonth.getWeeks(0))
 useWidget(WidgetData)
 </script>
 
 <template>
   <widget-wrapper>
     <div class="root flex flex-col">
-      <div class="header">
-        <div class="month">
+      <div class="flex items-baseline gap-2 py-3 px-4">
+        <div class="text-xl font-bold">
           {{ now.format('YYYY年MM月') }}
         </div>
-        <div class="weekOfYear">
+        <div class="text-xs">
           第{{ now.week() }}周
         </div>
+        <div class="ml-auto">
+          <Left />
+          <Right />
+        </div>
       </div>
-      <div class="weekday flex w-full justify-around">
-        <div>周日</div>
-        <div>周一</div>
-        <div>周二</div>
-        <div>周三</div>
-        <div>周四</div>
-        <div>周五</div>
-        <div>周六</div>
+      <div class="flex justify-around px-2 text-xs opacity-70">
+        <div>日</div>
+        <div>一</div>
+        <div>二</div>
+        <div>三</div>
+        <div>四</div>
+        <div>五</div>
+        <div>六</div>
       </div>
-      <div class="flex flex-col h-full justify-around">
+      <div class="flex flex-col h-full justify-around px-2">
         <div v-for="week in weeks" :key="`week-${week.getIndex()}`" class="flex w-full justify-around">
-          <div v-for="day in week.getDays()" :key="day.getDay()" class="flex w-full flex-col items-center content-center">
-            {{ day.getDay() }}
+          <div
+            v-for="day in week.getDays()" :key="day.getDay()" class="flex w-full flex-col items-center content-center"
+            :class="{ 'opacity-40': day.getMonth() != currentMonth }"
+          >
+            <CalendarDay :day="day" />
           </div>
         </div>
       </div>
